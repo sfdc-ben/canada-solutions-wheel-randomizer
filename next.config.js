f27b759fb24c5e857ed07b7fce519b4fc0d16d15
Ben Richards <ben.richards.sfdc@gmail.com>
2022-02-22 09:08:57 -0800

    Remove Webpack Config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  "resolutions": {
-    "webpack": "^5.0.0-beta.28"
-  },
-  webpack: (config, options) => {
-    config.experiments = {
-        "topLevelAwait": true
-    }
-    return config
-  },
+  // "resolutions": {
+  //   "webpack": "^5.0.0-beta.28"
+  // },
+  // webpack: (config, options) => {
+  //   config.experiments = {
+  //       "topLevelAwait": true
+  //   }
+  //   return config
+  // },
 }
